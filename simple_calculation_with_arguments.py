986b6c1751157d008d27d18624b05f7c0f74ce7c
Vera Kepski <besnjukv@web.de>
2021-06-08 10:30:32 +0200

    Initial commit

---
+++ simple_calculation_with_arguments.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("number_1", type=int, help="An integer")
+parser.add_argument("number_2", type=int, help="Another integer")
+parser.add_argument("--operation", choices=["div", "sub", "sum", "mult"])
+
+#args = parser.parse_args()
+# print(args)
+# print(args.number_1, args.number_2)
+
+if args.operation == "div":
+    result = args.number_1 / args.number_2
+elif args.operation == "sum":
+     result = args.number_1 + args.number_2  
+elif args.operation == "sub":
+    result = args.number_1 - args.number_2 
+elif args.operation == "mult":
+    result = args.number_1 * args.number_2 
+
+
+print(result)
